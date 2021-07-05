b109a972339205a5326f2467b0e89ec03bcc4ddd
maxim5 <smartmaxim@gmail.com>
2021-07-06 00:39:01 +0200

    add tests

--- README.md
+++ README.md
@@ -7,9 +7,10 @@ Super-fast and lightweight Java URL router.
 
 - **Fast**: highly efficient routing algorithm using hashtable index, prefix trie and finite state machine. No regexp matching.
 - **GC friendly**: minimal number of allocations during routing. No `String` and `char[]` waste.
-  **Small**: everything is one small library jar.
+- **Small**: whole library is one small jar.
 - **Scalable**: can handle thousands of rules without overhead.
 - **Simple**: supports string variables and wildcards.
 - **Lightweight**. No dependencies on other libraries. None. Zero.
 - **Adaptable**: API does not require or force any web framework or library. Works with any `String` or `char[]` input.
+- **Compatible**: Netty.
 - **Extensible**: supports options and extension points for new syntax.
--- test/io/routekit/util/CharBufferTest.java
+++ test/io/routekit/util/CharBufferTest.java
@@ -1,6 +1,5 @@
-package io.routekit;
+package io.routekit.util;
 
-import io.routekit.util.CharBuffer;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -24,6 +23,10 @@ public class CharBufferTest {
     @Test
     public void equals() {
         Assertions.assertEquals(new CharBuffer(""), new CharBuffer(""));
+        Assertions.assertEquals(new CharBuffer(""), new CharBuffer("foo", 0, 0));
+        Assertions.assertEquals(new CharBuffer(""), new CharBuffer("foo", 1, 1));
+        Assertions.assertEquals(new CharBuffer(""), new CharBuffer("foo", 2, 2));
+        Assertions.assertEquals(new CharBuffer(""), new CharBuffer("foo", 3, 3));
         Assertions.assertEquals(new CharBuffer("foo"), new CharBuffer("foo"));
         Assertions.assertEquals(new CharBuffer("foo"), new CharBuffer("foobar", 0, 3));
     }
@@ -31,6 +34,8 @@ public class CharBufferTest {
     @Test
     public void create_invalid_pointers() {
         Assertions.assertThrows(AssertionError.class, () -> new CharBuffer("foo", -1, 2));
+        Assertions.assertThrows(AssertionError.class, () -> new CharBuffer("foo", 2, 1));
+        Assertions.assertThrows(AssertionError.class, () -> new CharBuffer("foo", 0, 4));
         Assertions.assertThrows(AssertionError.class, () -> new CharBuffer("foo", 4, 4));
     }
 
@@ -52,6 +57,37 @@ public class CharBufferTest {
         Assertions.assertEquals(7, buffer.matchUntil(4, '-'));
         Assertions.assertEquals(7, buffer.matchUntil(7, '-'));
         Assertions.assertEquals(11, buffer.matchUntil(8, '-'));
+    }
+
+    @Test
+    public void matchCommon() {
+        Assertions.assertEquals(0, new CharBuffer("foo").matchCommon(new CharBuffer("bar")));
+        Assertions.assertEquals(2, new CharBuffer("bar").matchCommon(new CharBuffer("baz")));
+
+        Assertions.assertEquals(0, new CharBuffer("foo").matchCommon(new CharBuffer("foobar", 3, 6)));
+        Assertions.assertEquals(2, new CharBuffer("bar").matchCommon(new CharBuffer("barbaz", 3, 6)));
+        Assertions.assertEquals(0, new CharBuffer("foobar", 3, 6).matchCommon(new CharBuffer("foo")));
+        Assertions.assertEquals(2, new CharBuffer("barbaz", 3, 6).matchCommon(new CharBuffer("bar")));
+    }
+
+    @Test
+    public void matchCommon_empty() {
+        Assertions.assertEquals(0, new CharBuffer("").matchCommon(new CharBuffer("")));
+        Assertions.assertEquals(0, new CharBuffer("foo").matchCommon(new CharBuffer("")));
+        Assertions.assertEquals(0, new CharBuffer("").matchCommon(new CharBuffer("foo")));
+
+        Assertions.assertEquals(0, new CharBuffer("foo", 1, 2).matchCommon(new CharBuffer("foo", 3, 3)));
+        Assertions.assertEquals(0, new CharBuffer("xxx", 1, 1).matchCommon(new CharBuffer("xxx", 2, 2)));
+    }
+
+    @Test
+    public void matchCommon_common_prefix() {
+        Assertions.assertEquals(3, new CharBuffer("foo").matchCommon(new CharBuffer("foo")));
+        Assertions.assertEquals(3, new CharBuffer("foo").matchCommon(new CharBuffer("foobar")));
+        Assertions.assertEquals(3, new CharBuffer("foobar").matchCommon(new CharBuffer("foo")));
+
+        Assertions.assertEquals(3, new CharBuffer("foo").matchCommon(new CharBuffer("barfoo", 3, 6)));
+        Assertions.assertEquals(3, new CharBuffer("barfoo", 3, 6).matchCommon(new CharBuffer("foo")));
     }
 
     @Test
